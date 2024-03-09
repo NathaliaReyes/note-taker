bd089027bded8a3e16fe08c0c6e688b98cc87cde
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-09 11:26:53 -0700

    Adding GET route for retrieving all the notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,5 +2,12 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json')
+    .then((data) => 
+        res.json(JSON.parse(data))
+    );
+});
 
 module.exports = notes;
